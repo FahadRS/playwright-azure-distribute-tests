cb457bf3d66d947dc575765f2c91a73ae12d25cc
fahad.rehman <fahad.rehman@softechww.com>
2022-05-06 23:31:49 +0500

    updated the yaml

--- tests/second.spec.ts
+++ tests/second.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Page } from '@playwright/test';
 
 
 test.describe.serial('New Todo', () => {
-  test('second spec-first-test', async ({ page }) => {
+  test('third spec-first-test', async ({ page }) => {
       await page.goto("https://www.google.com");
   });
 
