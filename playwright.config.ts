dded90f5f5857e3bd0476df7365de03fea425a20
fahad.rehman <fahad.rehman@softechww.com>
2022-05-06 23:48:25 +0500

    updated the yaml

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,17 +30,8 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    browserName : "chromium"
   },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-  ],
 };
 
 export default config;
